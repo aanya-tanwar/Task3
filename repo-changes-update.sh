b56609dffe1b6b623a20bfa5eba05443b38bd401
aanya <aanya.tanwar@simformsolutions.com>
2025-02-06 18:31:57 +0530

    script added

---
+++ repo-changes-update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPO_DIR="/home/aanya.tanwar@simform.dom/task3
+" 
+LOG_FILE="/home/aanya.tanwar@simform.dom/git_update.log" 
+
+git fetch origin >> "$LOG_FILE" 2>&1
+
+git status | grep -q 'Your branch is behind'  
+
+if [ $? -eq 0 ]; then
+    echo "[ $(date) ] Your branch is behind. Pulling latest changes..." >> "$LOG_FILE"
+    git pull origin main >> "$LOG_FILE" 2>&1
+else
+    echo "[ $(date) ] Your branch is up to date with origin/main." >> "$LOG_FILE"
+fi
+
